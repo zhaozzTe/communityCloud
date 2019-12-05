be55b2343c5b4b8251296555d23c931fdaf22bb0
Cheis-CW <273312885@qq.com>
2019-12-05 22:21:50 +0800

    提交

--- pages/join-volunteer/join-volunteer.js
+++ pages/join-volunteer/join-volunteer.js
@@ -12,6 +12,7 @@ Page({
    */
   timer: null,
   data: {
+    islower:false,
     index: null,
     CategoryList: [],
     type: '',
@@ -65,26 +66,25 @@ Page({
     this.setData({
       params
     })
-    this.getNewsPage(params);
+    this.getNewsPage(params,false);
     this.getCategoryList({
       typeCode: 'ZHIYUAN_ZHAOMU'
     });
   },
 
-  getNewsPage: async function(params) {
+  getNewsPage: async function (params, islower) {
+    const { infos }= this.data;
     try {
      const res = await getNewsPage(params);
-      this.setData({
-        infos: res.data
+      islower?this.setData({
+        infos: [...infos, ...res.data] 
+      }) : this.setData({
+          infos: res.data
       })
      
     } catch (e) {}
   },
 
-  lower(e) {
-    console.log(111111);
-    if (!this.data.finish) this.getNewsPage(this.data.page + 1)
-  },
   getFocus: function(e) {
     this.setData({
       isHideSearchIcon: true
@@ -95,20 +95,23 @@ Page({
     let params = this.data.params;
     const _this = this;
     params.keyword = e.detail.value;
+    params.page = 1;
     this.setData({
       searchV: e.detail.value,
-      params
+      params,
     })
     this.timer && clearTimeout(this.timer);
     this.timer = setTimeout(function() {
-      _this.getNewsPage(params);
+      _this.getNewsPage(params, false);
     }, 1000)
 
   },
   myjoin(e) {
-    console.log(445445545, e);
+    if (!this.data.canAttend) {
+      return false;
+    }
     this.attend({
-      id: e.target.dataset.id
+      newsId: e.target.dataset.id
     });
   },
   async attend(params) {
@@ -116,7 +119,7 @@ Page({
       let res = await attend(params);
       console.log('6666666', res)
       wx.navigateTo({
-        url: '/pages/mySay-success/mySay-success',
+        url: '/pages/issue-success/issue-success'
       })
     } catch (e) {
       console.log(9999, e)
@@ -138,43 +141,47 @@ Page({
 
   bindPickerChange(e) {
     let {params,index} = this.data;
+    params.page = 1;
     params.category = this.data.CategoryList[e.detail.value].id;
     this.setData({
       params,
       index: e.detail.value,
-      // typeCode: this.data.CategoryList[index].id
     })
-    this.getNewsPage(params);
+    this.getNewsPage(params,false);
   },
 
   //上拉翻页
   onReachBottom: function() {
+    let { params } = this.data;
+    params.page = this.data.page + 1;
     // 最后一页了，取消下拉功能
     if (this.data.isLastPage) {
       return
     }
     this.setData({
-      page: this.data.page + 1
+      page: this.data.page + 1,
     });
-
+    this.getNewsPage(params, true);
   },
   noCanJoin(){
     let { params } = this.data;
+    params.page = 1;
     params.canAttend = false ;
     this.setData({
       canAttend: false,
       params,
     })
-    this.getNewsPage(params);
+    this.getNewsPage(params,false);
   },
   canJoin(){
     let { params } = this.data;
     params.canAttend = true;
+    params.page = 1;
     this.setData({
       canAttend:true,
       params,
     })
-    this.getNewsPage(params);
+    this.getNewsPage(params,false);
   }
 
 })
